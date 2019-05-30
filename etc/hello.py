707ad316cff8426d12f86a17707416bcbc254638
arpatutin <44804197+arpatutin@users.noreply.github.com>
2019-05-30 13:30:19 +0000

    Delete hello.py

--- etc/hello.py
+++
@@ -1,7 +0,0 @@
-def application(env, start_response):
-    a =  env.QUERY_STRING.replace('&', '\n')
-    code = '200 OK'
-    headers = [('Content-type', 'text/plain')]
-    start_response(code, headers)
-    return [ a ]
-
