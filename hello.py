fbee685baefab9244ec76920bbf6a5d5504e2387
arpatutin <44804197+arpatutin@users.noreply.github.com>
2019-05-30 13:43:16 +0000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 def application(env, start_response):
-    a =  [(i + '\n') for i in env['QUERY_STRING'].split('&')]
+    a =  [bytes(i + '\n', 'ascii') for i in environ['QUERY_STRING'].split('&')]
     code = '200 OK'
     headers = [('Content-type', 'text/plain')]
     start_response(code, headers)
