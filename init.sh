dc653be6df92eb978c2908cb11b636f8afde8101
Arseniy Patutin <arpatutin@gmail.com>
2019-06-06 11:22:33 +0400

    django/migrations, .gitignore, delete init.sh

--- init.sh
+++
@@ -1,6 +0,0 @@
-sudo ln /home/box/web/etc/nginx.conf /etc/nginx/conf.d/test.conf
-sudo gunicorn -c ~/web/etc/gunicorn.conf hello:application
-cd ask
-sudo django-admin runserver 0.0.0.0:8000
-cd ..
-sudo service nginx reload
