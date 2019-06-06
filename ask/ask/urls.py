dbe7c8bc287c2f1fa5ad635e804e5cc917e01f3d
arpatutin <44804197+arpatutin@users.noreply.github.com>
2019-06-06 11:05:39 +0400

    fix urls.py/question

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -22,7 +22,7 @@ urlpatterns = [
     url(r'^$', test),
     url(r'^login/', test),
     url(r'^signup/', test),
-    url(r'^question/(?P<int:id>)', test),
+    url(r'^question/(?P<int>[0-9]+)', test),
     url(r'^ask/', test),
     url(r'^popular/', test),
     url(r'^new/', test)
