41bb7cd07f2cc9dfcbe3d12597f6fac88a73c7fe
Arseniy Patutin <arpatutin@gmail.com>
2019-06-07 11:13:26 +0400

    db_mysql

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,6 +1,17 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
+import django.contrib.auth.models.User as User
+class Question(models.Model):
+    title = models.CharField(max_length=120)
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    rating = models.IntegerField()
+    author = models.OneToOneField(User)
+    likes = models.ManyToManyField(User)
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateTimeField()
+    question = models.ForeignKey(Question)
+    author = models.OneToOneField(User)
 
-# Create your models here.
