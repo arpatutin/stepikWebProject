83aa347d355116a4054c64bad61d430bfa550e67
Arseniy Patutin <arpatutin@gmail.com>
2019-06-10 15:18:53 +0400

    database fix+migrate

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.db import models
-import django.contrib.auth.models.User as User
+from django.contrib.auth.models import User
 class Question(models.Model):
     title = models.CharField(max_length=120)
     text = models.TextField()
     added_at = models.DateTimeField()
     rating = models.IntegerField()
     author = models.OneToOneField(User)
-    likes = models.ManyToManyField(User)
+    likes = [models.OneToOneField(User)]
 class QuestionManager(models.Manager):                                          
     def new():                                                              
             pass                                                            
