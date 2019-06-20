d9710d518e1e9df0062e33241b763a41cd194ebe
arpatutin <44804197+arpatutin@users.noreply.github.com>
2019-06-20 18:44:43 +0300

    Update models.py

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -2,6 +2,13 @@
 from __future__ import unicode_literals
 from django.db import models
 from django.contrib.auth.models import User
+
+class QuestionManager(models.Manager):                                          
+    def new(self):
+        self.order_by("-added_at")
+    def popular(self):
+        self.order_by("-rating")
+
 class Question(models.Model):
     title = models.CharField(max_length=120)
     text = models.TextField()
@@ -9,11 +16,11 @@ class Question(models.Model):
     rating = models.IntegerField(default=0)
     author = models.OneToOneField(User)
     likes = models.ManyToManyField(User, related_name="question_like_user")
-class QuestionManager(models.Manager):                                          
-    def new(self):
-        self.order_by("-added_at")
-    def popular(self):
-        self.order_by("-rating")
+    def __str__(self):
+        return self.title
+    def get_url(self):
+        return '/question/{}'.format(self.id)
+
 class Answer(models.Model):
     text = models.TextField()
     added_at = models.DateTimeField(auto_now_add=True)
