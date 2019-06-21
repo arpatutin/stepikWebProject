a57e1ce2dd0015de3c31ca95184df46166ba41df
arpatutin <44804197+arpatutin@users.noreply.github.com>
2019-06-21 08:49:49 +0300

    init commit

--- init.sh
+++ init.sh
@@ -1,9 +1,9 @@
-gunicorn --bind 0.0.0.0:8080 hello:application
 sudo /etc/init.d/mysql start
 mysql -u root -e "create database stepik;"
 mysql -u root -e "create user 'django'@'localhost' identified by 'mysite746';"
 mysql -u root -e "grant all privileges on stepik.* to 'django'@'localhost' with grant option;"
 ./ask/manage.py makemigrations
 ./ask/manage.py migrate
+./ask/manage.py runserver 0.0.0.0:8000 &
 sudo ln ~/web/etc/nginx.conf /etc/nginx/conf.d/test
 sudo service nginx reload
