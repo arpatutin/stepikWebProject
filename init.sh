cc1e27ab6afa008770a8b360465d0802746db78b
Arseniy Patutin <arpatutin@gmail.com>
2019-09-14 14:34:34 +0400

    init fix

--- init.sh
+++ init.sh
@@ -4,4 +4,4 @@ mysql -u root -e "create user 'django'@'localhost' identified by 'mysite746';"
 mysql -u root -e "grant all privileges on stepik.* to 'django'@'localhost' with grant option;"
 ./ask/manage.py makemigrations
 ./ask/manage.py migrate
-sudo ln ~/web/etc/nginx.conf /etc/nginx/conf.d/test
+sudo ln ~/web/etc/nginx.conf /etc/nginx/conf.d/test.conf
