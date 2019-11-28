ded68ff88c489b396f67c6ee6f06b1aecfaee926
Arseniy Patutin <arpatutin@gmail.com>
2019-11-28 19:07:13 +0400

    fixed init.sh

--- init.sh
+++ init.sh
@@ -3,8 +3,8 @@ mysql -u root -e "create database stepik;"
 mysql -u root -e "create user 'django'@'localhost' identified by 'mysite746';"
 mysql -u root -e "grant all privileges on stepik.* to 'django'@'localhost' with grant option;"
 ./ask/manage.py makemigrations
+./ask/manage.py makemigrations qa
 ./ask/manage.py migrate
 sudo ln ~/web/etc/nginx.conf /etc/nginx/conf.d/test.conf
 sudo rm -r /etc/nginx/sites-enabled/default
-gunicorn ask.wsgi -b 0.0.0.0:8000 &
 sudo service nginx restart
